c7d79e184c141542ab29cb8753a2d82e3b57e401
davidecaminati <davide.caminati@gmail.com>
2014-09-19 04:56:50 +0200

    First Update

---
+++ LettoreAudiolibri/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.listViewFolder = new System.Windows.Forms.ListView();
+            this.listViewMp3 = new System.Windows.Forms.ListView();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.listViewFolder);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listViewMp3);
+            this.splitContainer1.Panel2.Controls.Add(this.axWindowsMediaPlayer1);
+            this.splitContainer1.Size = new System.Drawing.Size(706, 545);
+            this.splitContainer1.SplitterDistance = 342;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // listViewFolder
+            // 
+            this.listViewFolder.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listViewFolder.Location = new System.Drawing.Point(0, 0);
+            this.listViewFolder.Name = "listViewFolder";
+            this.listViewFolder.Size = new System.Drawing.Size(342, 545);
+            this.listViewFolder.TabIndex = 0;
+            this.listViewFolder.UseCompatibleStateImageBehavior = false;
+            this.listViewFolder.View = System.Windows.Forms.View.List;
+            this.listViewFolder.SelectedIndexChanged += new System.EventHandler(this.listViewFolder_SelectedIndexChanged);
+            this.listViewFolder.KeyUp += new System.Windows.Forms.KeyEventHandler(this.listViewFolder_KeyUp);
+            // 
+            // listViewMp3
+            // 
+            this.listViewMp3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listViewMp3.Enabled = false;
+            this.listViewMp3.Location = new System.Drawing.Point(0, 0);
+            this.listViewMp3.Name = "listViewMp3";
+            this.listViewMp3.Size = new System.Drawing.Size(360, 545);
+            this.listViewMp3.TabIndex = 0;
+            this.listViewMp3.UseCompatibleStateImageBehavior = false;
+            this.listViewMp3.View = System.Windows.Forms.View.List;
+            this.listViewMp3.SelectedIndexChanged += new System.EventHandler(this.listViewMp3_SelectedIndexChanged);
+            this.listViewMp3.KeyUp += new System.Windows.Forms.KeyEventHandler(this.listViewMp3_KeyUp);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(61, 494);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(296, 48);
+            this.axWindowsMediaPlayer1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(706, 545);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Lettore Audiolibri";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListView listViewFolder;
+        private System.Windows.Forms.ListView listViewMp3;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+    }
+}
+
---
+++ LettoreAudiolibri/Form1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Speech.Synthesis;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        string percorso;
+        WMPLib.WindowsMediaPlayer Player;
+        bool onPause = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Parla("Audiolibri");
+            string folder = @System.IO.Path.GetDirectoryName(@System.Reflection.Assembly.GetExecutingAssembly().Location);
+            string path_fileconfig = folder + @"\config.txt";
+            //check if config file exist
+            if (!System.IO.File.Exists(path_fileconfig))
+            {
+            //create default file config
+                //first line for directory of the audiolibri directorys
+                string[] lines = { @"C:" };
+                // WriteAllLines creates a file, writes a collection of strings to the file,
+                // and then closes the file.
+                System.IO.File.WriteAllLines(path_fileconfig, lines);
+            }
+            // read config file
+            StreamReader sr =  System.IO.File.OpenText(path_fileconfig);
+            percorso = sr.ReadLine();
+            string[] elencolibri = Directory.GetDirectories(percorso);
+            //append to the elencolibri each foldername finded in the path of the audiolibri
+            foreach (string libro in elencolibri)
+            {
+                listViewFolder.Items.Add(System.IO.Path.GetFileNameWithoutExtension(libro));
+            }
+
+            if (listViewFolder.Items.Count > 0)
+            {
+                listViewFolder.Items[0].Selected = true;
+                listViewFolder.Select();
+            }
+            
+        }
+
+        static void Parla(string args)
+        {
+
+            // Initialize a new instance of the SpeechSynthesizer.
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+
+            // Configure the audio output. 
+            synth.SetOutputToDefaultAudioDevice();
+
+            synth.Speak(args);
+
+        }
+
+        private void listViewFolder_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listViewFolder.SelectedItems.Count == 1)
+            {
+                int selectedIndex = listViewFolder.SelectedIndices[0];
+                try
+                {
+                    Parla(listViewFolder.SelectedItems[0].Text.ToString());
+                }
+                catch { }
+            }
+        }
+
+        private string[] GetListOfFile(string path)
+        {
+            return Directory.GetFiles(path);
+        }
+
+        private void listViewFolder_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Enter:
+                    if ((listViewFolder.Items.Count > 0) && (listViewFolder.SelectedItems.Count > 0))
+                    {
+                        //popolate listViewMp3
+                        string[] listafile = GetListOfFile(Path.Combine(percorso, listViewFolder.SelectedItems[0].SubItems[0].Text));
+                        //delete old element in mp3 list
+                        listViewMp3.Clear();
+                        //add new element
+                        foreach (string mp3 in listafile)
+                        {
+                            listViewMp3.Items.Add(System.IO.Path.GetFileName(mp3),mp3);
+                        }
+                        //Focus on listViewMp3
+                        if (listViewMp3.Items.Count > 0)
+                        {
+                            listViewMp3.Enabled = true;
+                            listViewMp3.Items[0].Selected = true;
+                            listViewMp3.Select();
+                            listViewFolder.Enabled = false;
+                        }
+                        else 
+                        {
+                            Parla("non è possibile caricare i file audio");
+                            listViewFolder.Enabled = true;
+                        }
+                    }
+                    break;
+
+                case Keys.Escape:
+                    StopFile();
+                    Parla("Chiusura programma");
+                    Console.Beep();
+                    this.Close();
+                    break;
+
+                case Keys.Down:
+                    if (listViewFolder.Items.Count > 0)
+                    {
+                        if (listViewFolder.Items.Count - 1 == listViewFolder.Items.IndexOf(listViewFolder.SelectedItems[0]))
+                        {
+                            Parla("Ultimo elemento");
+                        }
+                    }
+                    break;
+
+
+                case Keys.Up:
+                    StopFile();
+
+                    if (listViewFolder.Items.Count > 0)
+                    {
+                        if (0 == listViewFolder.Items.IndexOf(listViewFolder.SelectedItems[0]))
+                        {
+                            Parla("primo elemento");
+                        }
+                    }
+                    break;
+            }
+        }
+
+        private void listViewMp3_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+
+                    StopFile();
+                    listViewFolder.Enabled = true;
+                    listViewMp3.Clear();
+                    listViewMp3.Enabled = false;
+
+                    if (listViewFolder.Items.Count > 0)
+                    {
+                        //listViewFolder.Items[0].Selected = true;
+                        listViewFolder.Focus ();
+                    }
+                    break;
+
+                case Keys.Space:
+                    PauseResumeFile();
+                    break;
+
+                case Keys.Down:
+                    if (listViewMp3.Items.Count > 0)
+                    {
+                        if (listViewMp3.Items.Count - 1 == listViewMp3.Items.IndexOf(listViewMp3.SelectedItems[0]))
+                        {
+                            Parla("Ultimo elemento");
+                        }
+                    }
+                    break;
+
+                case Keys.Escape:
+                    StopFile();
+                    Parla("Chiusura programma");
+                    Console.Beep();
+                    this.Close();
+                    break;
+
+
+                case Keys.Enter:
+                    if ((listViewMp3.Items.Count > 0) && (listViewMp3.SelectedItems.Count > 0))
+                    {
+                        //find path for mp3 file
+                        string percorsofile = Path.Combine(percorso, listViewFolder.SelectedItems[0].SubItems[0].Text, listViewMp3.SelectedItems[0].SubItems[0].Text);
+                        //stop eventualy other file
+                        StopFile();
+                        // open mp3file
+                        PlayFile(percorsofile);
+                    }
+                    else
+                    {
+                        Parla("non è possibile caricare i file audio");
+                        listViewFolder.Enabled = true;
+                    }
+                    break;
+
+                case Keys.Up:
+                    StopFile();
+
+                    if (listViewMp3.Items.Count > 0)
+                    {
+                        if (0 == listViewMp3.Items.IndexOf(listViewMp3.SelectedItems[0]))
+                        {
+                            Parla("primo elemento");
+                        }
+                    }
+                    break;
+            }
+        }
+
+        private void listViewMp3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listViewMp3.SelectedItems.Count == 1)
+            {
+                int selectedIndex = listViewMp3.SelectedIndices[0];
+                try
+                {
+                    Parla(listViewMp3.SelectedItems[0].Text.ToString());
+                }
+                catch { }
+            }
+        }
+
+        private void PlayFile(String url)
+        {
+            Player = new WMPLib.WindowsMediaPlayer();
+            Player.PlayStateChange +=
+                new WMPLib._WMPOCXEvents_PlayStateChangeEventHandler(Player_PlayStateChange);
+            Player.MediaError +=
+                new WMPLib._WMPOCXEvents_MediaErrorEventHandler(Player_MediaError);
+            Player.URL = url;
+            double pos = Player.controls.currentPosition;
+            pos += 10;
+            Player.controls.currentPosition = pos;
+
+        }
+
+        private void StopFile()
+        {
+            try
+            {
+                Player.controls.stop();
+                
+                onPause = false;
+            }
+            catch
+            { }
+        }
+
+        private void PauseResumeFile()
+        {
+            try
+            {
+                if (onPause)
+                { 
+                    /* Pause the Player. */
+                    Player.controls.play();
+                    onPause = false;
+                }
+                else
+                {
+                    Player.controls.pause();
+                    onPause = true;
+                }
+            }
+            catch
+            { }
+        }
+
+
+        private void Player_PlayStateChange(int NewState)
+        {
+            if ((WMPLib.WMPPlayState)NewState == WMPLib.WMPPlayState.wmppsStopped)
+            {
+               // to do 
+                //for eache file save the position and ask if continue from this position on load
+                string posizione = Player.controls.currentPositionString;
+            }
+        }
+
+        private void Player_MediaError(object pMediaObject)
+        {
+            Parla("errore nel caricamento del file");
+        }
+
+
+    }
+}
